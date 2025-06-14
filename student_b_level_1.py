f0dec8e441034e8e857847ffe666fd5db206a617
s4150961 <s4150961@student.rmit.edu.au>
2025-06-14 20:16:08 +1000

    changes 2

--- student_b_level_1.py
+++ student_b_level_1.py
@@ -1,21 +1,84 @@
 def get_page_html(form_data):
-    print("About to return page home page...")
-    page_html="""<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <title>Database Web-App Demo</title>
-    </head>
-    <body>
-        <h1>Page 1B - Hello, World!</h1>
-        <p>This is the first dynamically generated page!</p>
-        <p><a href="/">Go to Page 1A</a></p>
-        <p><a href="/page2a">Go to Page 2A</a></p>
-        <p><a href="/page3a">Go to Page 3A</a></p>
-        <p><a href="/page1b">Go to Page 1B</a></p>
-        <p><a href="/page2b">Go to Page 2B</a></p>
-        <p><a href="/page3b">Go to Page 3B</a></p>
-        <img src="images/rmit.png" style="width: 30%; height: auto;">
-    </body>
-    </html>
-    """
+    print("About to return home page...")
+    page_html = """<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Weather Insights Application</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 10px;
+            background-color: #f9f9f9;
+        }
+        header {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            border-bottom: 2px solid #ddd;
+            padding-bottom: 10px;
+        }
+        header h1 {
+            color: black;
+        }
+        nav a {
+            margin-left: 30px;
+            text-decoration: none;
+            color: black;
+            font-weight: bold;
+        }
+        .content-container {
+            margin-left: 80px;
+            margin-right: 80px;
+            /* optional: margin-top: 20px; if you want space below header */
+        }
+        .section {
+            margin-top: 30px;
+        }
+        .section h2 {
+            color: #222;
+            border-bottom: 2px solid #ccc;
+            padding-bottom: 5px;
+        }
+        .container {
+            display: flex;
+            justify-content: space-between;
+            gap: 30px;
+            flex-wrap: wrap;
+        }
+        .box {
+            flex: 1;
+            background-color: #fff;
+            padding: 20px;
+            border: 1px solid #bbb;
+            border-radius: 5px;
+            min-width: 300px;
+        }
+        .image-box {
+            flex: 1;
+            text-align: center;
+            min-width: 300px;
+        }
+        .image-box img {
+            max-width: 100%;
+            height: auto;
+            object-fit: contain;
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <h1>🌤️    AUSTRALIAN WEATHER Insight</h1>
+        <nav class="nav-bar">
+            <a href="/">Home</a>
+            <a href="/page2a">Page 1B</a>
+            <a href="/page3a">Page 1C</a>
+            <a href="/page1b">Page 2A</a>
+            <a href="/page2b">Page 2B</a>
+            <a href="/page3b">Page 2C</a>
+        </nav>
+    </header>
+
+</html>
+"""
     return page_html
--- student_b_level_3.py
+++ student_b_level_3.py
@@ -1,99 +1,84 @@
-import pyhtml
 def get_page_html(form_data):
-    print("About to return page 3")
-    #Create the top part of the webpage
-    #Note that the drop down list ('select' HTML element) has been given the name "var_star"
-    #We will use this same name in our code further below to obtain what the user selected.
-    page_html="""<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <title>Page 3B - Forms, databases and advanced queries</title>
-    </head>
-    <body>
-        <h1>Welcome to Page 3B!</h1>
-        <p>List the movies based on the star</p>
-        <form action="/page3b" method="GET">
-        
-          <label for="var_star">Movie Star</label>
-          <select name="var_star" multiple>"""
-    #Before you read further, play around with the web-page and note how selecting a star name from the first
-    #drop down list populates the second drop down list with the movies in which they have featured.
-    
-    #Note that although we see the name of the movie star in the first drop down list, when a star is selected and submitted,
-    #our program receives the star's ID (primary key).
-    
-    ################################ Movie star drop down list is generated below ######################################
-          
+    print("About to return home page...")
+    page_html = """<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Weather Insights Application</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 10px;
+            background-color: #f9f9f9;
+        }
+        header {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            border-bottom: 2px solid #ddd;
+            padding-bottom: 10px;
+        }
+        header h1 {
+            color: black;
+        }
+        nav a {
+            margin-left: 30px;
+            text-decoration: none;
+            color: black;
+            font-weight: bold;
+        }
+        .content-container {
+            margin-left: 80px;
+            margin-right: 80px;
+            /* optional: margin-top: 20px; if you want space below header */
+        }
+        .section {
+            margin-top: 30px;
+        }
+        .section h2 {
+            color: #222;
+            border-bottom: 2px solid #ccc;
+            padding-bottom: 5px;
+        }
+        .container {
+            display: flex;
+            justify-content: space-between;
+            gap: 30px;
+            flex-wrap: wrap;
+        }
+        .box {
+            flex: 1;
+            background-color: #fff;
+            padding: 20px;
+            border: 1px solid #bbb;
+            border-radius: 5px;
+            min-width: 300px;
+        }
+        .image-box {
+            flex: 1;
+            text-align: center;
+            min-width: 300px;
+        }
+        .image-box img {
+            max-width: 100%;
+            height: auto;
+            object-fit: contain;
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <h1>🌤️    AUSTRALIAN WEATHER Insight</h1>
+        <nav class="nav-bar">
+            <a href="/">Home</a>
+            <a href="/page2a">Page 1B</a>
+            <a href="/page3a">Page 1C</a>
+            <a href="/page1b">Page 2A</a>
+            <a href="/page2b">Page 2B</a>
+            <a href="/page3b">Page 2C</a>
+        </nav>
+    </header>
 
-    #Put the query together.
-    query = "select * from star;"
-    
-    #Run the query on the movies.db in the 'database' folder and get the results
-    #Note that all results are in the str data type first, even if they had different types in the database.
-    results = pyhtml.get_results_from_query("database/movies.db",query)
-    
-    #Get the value or values in the HTML dropdown list that we named "var_star" or None no data was sent through.
-    #If the user selects multiple movie stars on the web_page, we will have multiple values.
-    var_star = form_data.get('var_star')
-    
-    print("var_star selected on webpage is: ",var_star)
-    
-    #If the user had selected one or more stars on the web-page, convert their IDs to int
-    if(var_star!=None):
-        #Take the list of strings and convert the items to ints
-        var_star = [int(star) for star in var_star]
-    
-    #Create the drop down list of movie stars
-    for row in results:
-        #row[0] is the ID/primary key of the movie stars
-        page_html+='<option value="'+str(row[0])+'"'
-        #If there was a previous selection of a star on the web page, have them selected by default to be user-friendly.
-        if var_star!=None and row[0]==var_star[0]:
-            page_html+=' selected="selected"'
-            
-        #row[1] is the name of the star, which is what the user sees in the drop down list.
-        page_html+='>'+str(row[1])+'</option>'
-        
-    page_html+="</select><br><br>"
-
-
-
-    ################################ Movies drop down list is generated below ##########################################
-    
-    page_html+="""<label for="var_movie">Movie</label>
-    <select name="var_movie" """
-
-    #We create this drop down list only if a movie star was chosen
-    if var_star!=None:
-        #Query for getting the list of movie IDs and their titles by star
-        query ="""SELECT movie.mvnumb, movie.mvtitle 
-        FROM movie 
-        JOIN movstar ON movie.mvnumb = movstar.mvnumb """
-        query+=f"WHERE movstar.starnumb = {var_star[0]};"
-
-        #Run query and get results
-        results = pyhtml.get_results_from_query("database/movies.db",query)
-        page_html+=" >"
-        #row[0] is the movie ID (primary key) and row[1] is the movie title
-        for row in results:
-            page_html+='<option value="'+str(row[0])+'"\>'+str(row[1])+'</option>'
-    else:
-        #If no movie star was chosen, we create a dummy list and make it disabled so the user sees the movie drop down
-        #but they can't access it.
-        page_html+="disabled>"
-        page_html+='<option>Choose a star</option>'
-    page_html+="</select><br><br>"
-
-    page_html+="""
-    <input type="submit" value="Show starred movies">
-    </form>
-        <p><a href="/">Go to Page 1A</a></p>
-        <p><a href="/page2a">Go to Page 2A</a></p>
-        <p><a href="/page3a">Go to Page 3A</a></p>
-        <p><a href="/page1b">Go to Page 1B</a></p>
-        <p><a href="/page2b">Go to Page 2B</a></p>
-        <p><a href="/page3b">Go to Page 3B</a></p>
-    </body>
-    </html>
-    """
+</html>
+"""
     return page_html
